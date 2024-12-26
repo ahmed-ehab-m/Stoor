038a09196d3f7298b779610c6a565de229618d6d
Ahmed Ehab <ahmedehab56200@gmail.com>
2024-12-26 16:43:09 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Bookly App',
+      home: const SplashView(),
+    );
+  }
+}
