16edb945fbeb73c0dc9063773efa8e3c8d74afa6
Ahmed Ehab <ahmedehab56200@gmail.com>
2025-04-22 18:54:52 +0200

    refactor book details view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,9 +1,6 @@
-import 'package:bookly_app/Features/home/presentation/views/widgets/book_action.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/books_details_section.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/similar_books_list_view.dart';
-import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -11,7 +8,7 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
+    // var width = MediaQuery.of(context).size.width;
     //to use Expanded widget in the column, we need to wrap the column with a sliver widget
     //and use SliverFillRemaining to make the column take all the available space
     return CustomScrollView(
@@ -24,41 +21,7 @@ class BookDetailsViewBody extends StatelessWidget {
             child: Column(
               children: [
                 CusomAppBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-                  child: CustomBookImage(),
-                ),
-                const SizedBox(
-                  height: 43,
-                ),
-                Text(
-                  'Fortress Blood',
-                  style:
-                      Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 6,
-                ),
-                Opacity(
-                  opacity: 0.7,
-                  child: Text(
-                    'L.D. GOFFIGAN',
-                    style: Styles.textStyle18.copyWith(
-                      fontWeight: FontWeight.w500,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                BookRating(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                const SizedBox(
-                  height: 37,
-                ),
-                BookAction(),
+                BookDetailsSection(),
                 Expanded(
                   //to make the description take all the available space (Responsive)
                   child: const SizedBox(
@@ -66,22 +29,7 @@ class BookDetailsViewBody extends StatelessWidget {
                         50, ////minimum height between the book action and the description
                   ),
                 ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also like ',
-                    style: Styles.textStyle14.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                SimilarBooksListView(),
-                const SizedBox(
-                  height: 40,
-                ),
+                SimilarBooksSection(),
               ],
             ),
           ),
