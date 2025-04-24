d250ce52a3c55a7e01fb8be9e052b3c9c0c9fff0
Ahmed Ehab <ahmedehab56200@gmail.com>
2025-04-24 02:27:16 +0200

    create newest books cubit states

---
+++ lib/Features/home/presentation/manager/newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,28 @@
+part of 'newest_books_cubit.dart';
+
+sealed class NewestBooksState extends Equatable {
+  const NewestBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class NewestBooksInitial extends NewestBooksState {}
+
+final class NewestBooksLoading extends NewestBooksState {}
+
+final class NewestBooksSuccess extends NewestBooksState {
+  final List<BookModel> books;
+  const NewestBooksSuccess(this.books);
+
+  @override
+  List<Object> get props => [books];
+}
+
+final class NewestBooksFailure extends NewestBooksState {
+  final String errorMessage;
+  const NewestBooksFailure(this.errorMessage);
+
+  @override
+  List<Object> get props => [errorMessage];
+}
